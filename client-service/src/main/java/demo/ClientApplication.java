140c024b989c520da23195e619c112c897af345c
Nabil Belakbir <nbelakbir@octo.com>
2017-03-22 12:39:19 +0000

    First commit ARCHI 2

---
+++ client-service/src/main/java/demo/ClientApplication.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * The {@link ClientApplication} is a cloud-native Spring Boot application that manages
+ * a bounded context for @{link Customer}, @{link Account}, @{link CreditCard}, and @{link Address}
+ *
+ * @author Kenny Bastani
+ * @author Josh Long
+ */
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableEurekaClient
+public class ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
